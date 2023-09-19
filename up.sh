b1c4fe734565b457da586558734b9bfe6b37b79c
GGarrido <guillermo.garrido.edu@gmail.com>
2023-09-19 13:47:00 +0200

    feat: Implements services selection script

--- up.sh
+++ up.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Comprueba si existe el fichero .env
+# Comprueba si existe el fichero .env y .services
 test ! -f ./.env && { echo -e "\033[0;31m[ERROR]\033[0m No existe el fichero .env. Saliendo.." ; exit; }
+test ! -f ./.services && { echo -e "\033[0;31m[ERROR]\033[0m No existe el fichero .services. Saliendo.." ; exit; }
 # Carga las variables de entorno del fichero .env
-set -o allexport && source ./.env && set +o allexport
+set -o allexport && source ./.env && source ./.services && set +o allexport
 # Muestra un mensaje de pull
 echo "Git Pull realizado." 
 # Ejecuta el pull rebase para actualizar el repositorio
@@ -11,4 +12,5 @@ git pull --rebase
 git stash pop
 git stash drop
 # Ejecuta el docker compose
-exec docker compose up -d
+echo -e "\033[0;32m[INFO]\033[0m Arrancando los servicios: ${SERVICES[@]}"
+exec docker compose up -d "$@" "${SERVICES[@]}"
