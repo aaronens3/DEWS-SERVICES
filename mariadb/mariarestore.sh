f5f2b1d348e6e0542ae5e62a240438a499a6ee7c
Guillrmo <guillermo.garrido.edu@gmail.com>
2023-09-22 10:12:30 +0200

    fix: check if mariadb backup exists

--- mariadb/mariarestore.sh
+++ mariadb/mariarestore.sh
@@ -1,1 +1,4 @@
-mysql -u root -p"${MARIADB_ROOT_PASSWORD}" < /docker-entrypoint-initdb.d/db-dump/mariadb-dump.sql
+if [ -f "/docker-entrypoint-initdb.d/db-dump/mariadb-dump.sql" ]; 
+then 
+    mysql -u root -p"${MARIADB_ROOT_PASSWORD}" < /docker-entrypoint-initdb.d/db-dump/mariadb-dump.sql
+fi
