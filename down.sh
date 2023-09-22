8160a771bb74675bd0d77aac6556470277158bea
Guillrmo <guillermo.garrido.edu@gmail.com>
2023-09-22 13:23:24 +0200

    feat: check all www repos

--- down.sh
+++ down.sh
@@ -3,14 +3,28 @@
 test ! -f ./.env && { echo -e "\033[0;31m[ERROR]\033[0m No existe el fichero .env. Saliendo.." ; exit; }
 # Carga las variables de entorno del fichero .env
 set -o allexport && source ./.env && set +o allexport
-# Comprueba si hay cambios en el repositorio
+
+# Función para recorrer carpetas y ejecutar stash y push
+function recorrer_y_stash_push() {
+  for carpeta in "${SERVER_PATH}/www/*"; do
+    if [ -d "$carpeta/.git" ]; then
+      # Esta carpeta es un repositorio Git
+      echo "Git stash realizado en $carpeta." 
+      git stash push -u
+    fi
+  done
+}
+
+
+# Comprueba si hay cambios en el repositorio principal
 if [[ `git status --porcelain` ]]; 
 then
   docker-compose exec -T "${ME_CONFIG_MONGODB_SERVER}" mongodump --out ./docker-entrypoint-initdb.d/db-dump -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin
   docker-compose exec mariadb mysqldump -u root -p"${MARIADB_ROOT_PASSWORD}" --all-databases > ./mariadb/db-dump/mariadb-dump.sql
   # Ejecuta el stash push para guardar los cambios
-  echo "Git stash realizado." 
+  echo "Git stash realizado repositorio principal." 
   git stash push -u
+  recorrer_y_stash_push
 fi
 # Ejecuta el docker compose down para parar y borrar los contenedores
 exec docker compose down -v
--- up.sh
+++ up.sh
@@ -4,6 +4,22 @@ test ! -f ./.env && { echo -e "\033[0;31m[ERROR]\033[0m No existe el fichero .en
 test ! -f ./.services && { echo -e "\033[0;31m[ERROR]\033[0m No existe el fichero .services. Saliendo.." ; exit; }
 # Carga las variables de entorno del fichero .env
 set -o allexport && source ./.env && source ./.services && set +o allexport
+# Función para recorrer carpetas y ejecutar stash pop, drop y pull
+function recorrer_y_stash_pull() {
+  for carpeta in "${SERVER_PATH}/www/*"; do
+    if [ -d "$carpeta/.git" ]; then
+        # Muestra un mensaje de pull
+        echo "Git Pull realizado en $carpeta." 
+        # Ejecuta el pull rebase para actualizar el repositorio
+        git pull --rebase 
+        # Ejecuta el stash pop y drop para actualizar y eliminar los cambios locales
+        git stash pop
+        git stash drop
+    fi
+  done
+}
+
+
 # Muestra un mensaje de pull
 echo "Git Pull realizado." 
 # Ejecuta el pull rebase para actualizar el repositorio
@@ -11,6 +27,7 @@ git pull --rebase
 # Ejecuta el stash pop y drop para actualizar y eliminar los cambios locales
 git stash pop
 git stash drop
+recorrer_y_stash_pull
 # Ejecuta el docker compose
 echo -e "\033[0;32m[INFO]\033[0m Arrancando los servicios: ${SERVICES[@]}"
 exec docker compose up -d "$@" "${SERVICES[@]}"
